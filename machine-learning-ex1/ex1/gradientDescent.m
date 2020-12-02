8f04a68735c2e3b63372d4c05e197401745f7585
Sharon ushus babu <ubsharon93@gmail.com>
2020-12-02 21:40:52 +0100

    week3 file logistic regression added

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,7 +16,7 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    HypoPredvals = X * theta; % Predicted Hypothetical Theta values
+    HypoPredvals = X * theta; % Predicted Hypothetical Theta values, where 'X' is design matrix
     theta0 = theta(1) - alpha / m * sum((HypoPredvals - y) .* X(:,1));
     theta1 = theta(2) - alpha / m * sum((HypoPredvals - y) .* X(:,2)); 
     theta = [theta0; theta1];
